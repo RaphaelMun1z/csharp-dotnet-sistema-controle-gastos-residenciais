aabad8e9e215c6eb11ff873fec4ca719648609ec
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-07-19 03:23:57 -0300

    feat: adiciona service e controller de pessoas

--- SistemaControleGastosResidenciais/Controllers/PersonController.cs
+++ SistemaControleGastosResidenciais/Controllers/PersonController.cs
@@ -1,20 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Numerics;
+using SistemaControleGastosResidenciais.Entities;
+using SistemaControleGastosResidenciais.Services.Interfaces;
 
-namespace SistemaControleGastosResidenciais.Controllers
-{
+// Utilizei um pré-fixo "api" nas endpoints da API, para indicar que se trata de uma API Rest
+// Também utilizei a versão "v1" para indicar que é a primeira versão da API.
+// Isso permite que futuras versões da API sejam lançadas sem quebrar a compatibilidade com clientes existentes.
+namespace SistemaControleGastosResidenciais.Controllers {
     [ApiController]
-    [Route("api/people/v1")]
-    public class PersonController : ControllerBase
-    {
-        [HttpGet]
-        public IActionResult FindAll(string? name, int? age)
-        {
-            if(age <= 0)
-            {
-                return BadRequest("Informe uma idade válida! Igual ou maior a 1.");
-            }
-            return Ok("1");
+    [Route("api/people")]
+    public class PersonController : ControllerBase {
+        private IPersonServices _personService;
+
+        // O construtor recebe uma instância do serviço de pessoas, que é injetada pelo mecanismo de injeção de dependência
+        public PersonController(IPersonServices personService) {
+            _personService = personService;
+        }
+
+        [HttpGet("v1")]
+        public IActionResult Get() {
+            // Chama o método de busca presente no serviço, que retorna a lista de todas as pessoas registradas
+            List<Person> peopleList = _personService.FindAll();
+            return Ok(peopleList);
+        }
+
+        [HttpGet("v1/{id}")]
+        public ActionResult<Person> Get(Guid id) {
+            // Chama o método de busca presente no serviço, passando o ID da pessoa a ser buscada
+            // Retorna a pessoa encontrada, com status 200, indicando que a operação foi bem sucedida
+            Person personFound = _personService.FindById(id);
+            return Ok(personFound);
+        }
+
+        [HttpPost("v1")]
+        public IActionResult Post([FromBody] Person person) {
+            // Solicita a criação da pessoa via serviço, e recebe a instância cadastrada
+            // Retorna status 200, indicando que a pessoa foi cadastrada com sucesso
+            Person createdPerson = _personService.Create(person);
+            return Ok(createdPerson);
+        }
+
+
+        [HttpDelete("v1/{id}")]
+        public IActionResult DeleteById(Guid id) {
+            // Chama o método de deletar presente no serviço, passando o ID da pessoa a ser deletada
+            _personService.Delete(id);
+
+            // Retorna 'no content', indicando que a operação foi bem sucedida, mas sem conteúdo para retornar
+            return NoContent();
         }
     }
 }
---
+++ SistemaControleGastosResidenciais/Services/Interfaces/IPersonServices.cs
@@ -0,0 +1,10 @@
+﻿using SistemaControleGastosResidenciais.Entities;
+
+namespace SistemaControleGastosResidenciais.Services.Interfaces {
+    public interface IPersonServices {
+        Person FindById(Guid id);
+        List<Person> FindAll();
+        Person Create(Person person);
+        void Delete(Guid id);
+    }
+}
