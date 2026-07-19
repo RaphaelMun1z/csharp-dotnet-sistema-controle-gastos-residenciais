7021f3b06a59d12b700f504455ce048606828f67
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-07-18 21:32:31 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaControleGastosResidenciais/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+
+namespace SistemaControleGastosResidenciais.Controllers
+{
+    [ApiController]
+    [Route("api/people/v1")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult FindAll(string? name, int? age)
+        {
+            if(age <= 0)
+            {
+                return BadRequest("Informe uma idade válida! Igual ou maior a 1.");
+            }
+            return Ok("1");
+        }
+    }
+}
---
+++ SistemaControleGastosResidenciais/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Principal;
+using System.Transactions;
+
+namespace SistemaControleGastosResidenciais.Entities
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+
+        public Account? Account { get; set; }
+
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
